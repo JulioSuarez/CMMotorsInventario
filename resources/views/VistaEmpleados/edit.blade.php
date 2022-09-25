ebe0021384182f6392aa171a52d48a09a8c94cac
JulioSuarez <julio.suarez91@gmail.com>
2022-09-25 19:56:20 -0400

    estilos

--- resources/views/VistaEmpleados/edit.blade.php
+++ resources/views/VistaEmpleados/edit.blade.php
@@ -70,6 +70,6 @@
         </div>
 
         <button type="submit"
-            class="md:w-32 bg-blue-600 dark:bg-gray-100 text-white dark:text-gray-800 font-bold py-3 px-6 rounded-lg mt-4 hover:bg-blue-500 dark:hover:bg-gray-200 transition ease-in-out duration-300">Registrar</button>
+            class="md:w-32 bg-blue-600 dark:bg-gray-100 text-white dark:text-gray-800 font-bold py-3 px-6 rounded-lg mt-4 hover:bg-blue-500 dark:hover:bg-gray-200 transition ease-in-out duration-300">Guardar</button>
     </form>
 @endsection
