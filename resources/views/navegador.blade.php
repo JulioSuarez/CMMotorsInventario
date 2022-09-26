1d2c62417cd0c1a7fd4db86ddb72cd7dc5109a44
JulioSuarez <julio.suarez91@gmail.com>
2022-09-25 22:28:20 -0400

    Revert "estilos"
    This reverts commit ebe0021384182f6392aa171a52d48a09a8c94cac.

--- resources/views/navegador.blade.php
+++ resources/views/navegador.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CM Motor's Import Export</title>
-    {{-- @vite('resources/css/app.css') --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" /> <!-- este es de tailwin-->
+    @vite('resources/css/app.css') 
+    {{--<link rel="stylesheet" href="{{ asset('css/app.css') }}" />--}} <!-- este es de tailwin-->
     {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> <!-- no existe este archivo--> --}}
 </head>
 
